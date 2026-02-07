76058e3e3c5d97fc607a6d94799d3f511a5d583b
Yasuhiro Arakawa <arakawa@matsukura-sangyo.co.jp>
2026-02-07 22:38:14 +0900

    Use custom domain base URL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://yshr1920.github.io',
+  url: 'https://cotomy.net',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/cotomy/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
