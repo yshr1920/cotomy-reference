2ecd8215b2c18af0b5b951b730d922cdbeda10e9
Yasuhiro Arakawa <arakawa@matsukura-sangyo.co.jp>
2026-02-07 21:32:58 +0900

    Update site config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,14 +15,14 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://yshr1920.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/cotomy/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'cotomy', // Usually your GitHub org/user name.
+  organizationName: 'yshr1920', // Usually your GitHub org/user name.
   projectName: 'cotomy', // Usually your repo name.
 
   onBrokenLinks: 'throw',
